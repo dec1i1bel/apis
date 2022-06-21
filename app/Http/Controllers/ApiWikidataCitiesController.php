645689a40dcbe0a4a1c6710df86000bc5bc13ac2
v.balabanov <v.balabanov@ddemo.ru>
2022-06-21 08:09:58 +0300

    saving cities.json to storage

--- app/Http/Controllers/ApiWikidataCitiesController.php
+++ app/Http/Controllers/ApiWikidataCitiesController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\WikidataCities;
+use Illuminate\Support\Facades\Storage;
 
 class ApiWikidataCitiesController extends Controller
 {
     public function getCities()
     {
         $json = WikidataCities::all()->toJson(JSON_UNESCAPED_UNICODE);
-        $fp = fopen('results.json', 'w');
+        $fp = fopen('../storage/app/public/jsons/cities.json', 'w');
         fwrite($fp, $json);
         fclose($fp);
 
